622739b87dc7731063bd931af93966706c6652c8
kevdevbot <kevdevbot@gmail.com>
2021-01-17 15:15:13 -0700

    Updated build styles

--- src/app/services/sims4-build-style.service.ts
+++ src/app/services/sims4-build-style.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Sims4BuildPackService } from './sims4-build-pack.service';
 import { getRandomInt } from '../util/get-random-int';
+
+class StyleCondition {
+  constructor(public pack: string = '', public styles: string[] = []) {}
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class Sims4BuildStyleService {
-  buildStyles: string[] = [
+  constructor(sims4packs: Sims4BuildPackService) {
+    sims4packs.packs.subscribe((val) => {
+      const updatedTraits = [];
+      for (const condition of this.stylesWithConditions) {
+        if (val.get(condition.pack)?.enabled == true) {
+          for (const trait of condition.styles) {
+            updatedTraits.push(trait);
+          }
+        }
+      }
+      this.styles = [...updatedTraits, ...this.baseStyles];
+    });
+  }
+
+  styles: string[] = [];
+
+  private baseStyles: string[] = [
     'Adobe House',
     'A-Frame House',
     'All Glass House',
@@ -37,16 +58,24 @@ export class Sims4BuildStyleService {
     'Small 100-tile House',
     'Suburban House',
     'Tiny 64-tile House',
-    'Traditional Japanese House',
-    'Trailer',
+    'Trailer or Trailer Park',
     'Treehouse',
     'Tudor House',
-    'Underground House',
+    'Underground Bunker',
     'Victorian House',
     'Victorian Row House',
+    'Suburban Townhouse',
+    'Rustic Country House',
+    'Italianate House',
+  ];
+
+  private stylesWithConditions: StyleCondition[] = [
+    new StyleCondition('Snowy Escape', ['Traditional Japanese House']),
+    new StyleCondition('Eco Lifestyle', ['Eco Friendly House']),
+    new StyleCondition('Paranormal', ['Haunted House']),
   ];
 
   getRandom(): string {
-    return this.buildStyles[getRandomInt(0, this.buildStyles.length - 1)];
+    return this.styles[getRandomInt(0, this.styles.length - 1)];
   }
 }
