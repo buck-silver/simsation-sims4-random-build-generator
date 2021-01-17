91061bec95fe7d25f32f227ebea7c285c5524500
kevdevbot <kevdevbot@gmail.com>
2021-01-17 09:03:00 -0700

    Removed redundant material table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
 import { SimsationLogoComponent } from './components/simsation-logo/simsation-logo.component';
 import { SimsationSocialComponent } from './components/simsation-social/simsation-social.component';
 import { Sims4PackSettingsComponent } from './components/sims4-pack-settings/sims4-pack-settings.component';
 import { Sims4BuildSuggestionComponent } from './components/sims4-build-suggestion/sims4-build-suggestion.component';
+import { Sims4SimCountSettingsComponent } from './components/sims4-sim-count-settings/sims4-sim-count-settings.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +20,7 @@ import { Sims4BuildSuggestionComponent } from './components/sims4-build-suggesti
     SimsationSocialComponent,
     Sims4PackSettingsComponent,
     Sims4BuildSuggestionComponent,
+    Sims4SimCountSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,7 +28,6 @@ import { Sims4BuildSuggestionComponent } from './components/sims4-build-suggesti
     MatButtonModule,
     MatIconModule,
     MatCheckboxModule,
-    MatTableModule,
     MatDividerModule,
   ],
   providers: [],
