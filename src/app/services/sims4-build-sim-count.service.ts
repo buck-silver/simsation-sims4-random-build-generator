c9f9c9341343942138c6357219407b19d44494dd
kevdevbot <kevdevbot@gmail.com>
2021-01-17 12:19:38 -0700

    Updated sim range logic

--- src/app/services/sims4-build-sim-count.service.ts
+++ src/app/services/sims4-build-sim-count.service.ts
@@ -1,22 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Input } from '@angular/core';
 import { getRandomInt } from '../util/get-random-int';
+
+export const MIN_SIM_COUNT = 1;
+export const MAX_SIM_COUNT = 8;
 
 @Injectable({
   providedIn: 'root',
 })
 export class Sims4BuildSimCountService {
-  simCounts: string[] = [
-    '1 Sim',
-    '2 Sims',
-    '3 Sims',
-    '4 Sims',
-    '5 Sims',
-    '6 Sims',
-    '7 Sims',
-    '8 Sims',
-  ];
+  private _min = MIN_SIM_COUNT;
+  get min(): number {
+    return this._min;
+  }
+  @Input() set min(val: number) {
+    val = Math.floor(val);
+    if (val < MIN_SIM_COUNT) {
+      throw new Error(
+        `Sim min count cannot be set below minimum of ${MIN_SIM_COUNT}`
+      );
+    } else if (val > this.max) {
+      throw new Error(
+        `Sim min count cannot be set above maximum of ${this.max}`
+      );
+    } else {
+      this._min = val;
+    }
+  }
+
+  private _max = MAX_SIM_COUNT;
+  get max(): number {
+    return this._max;
+  }
+  @Input() set max(val: number) {
+    val = Math.floor(val);
+    if (val > MAX_SIM_COUNT) {
+      throw new Error(
+        `Sim max count cannot be set above maximum of ${MAX_SIM_COUNT}`
+      );
+    } else if (val < this.min) {
+      throw new Error(
+        `Sim max count cannot be set below minimum of ${this.min}`
+      );
+    } else {
+      this._max = val;
+    }
+  }
 
   getRandom(): string {
-    return this.simCounts[getRandomInt(0, this.simCounts.length - 1)];
+    const simCount = getRandomInt(this.min, this.max);
+    return `${simCount} ${simCount > 1 ? 'Sims' : 'Sim'}`;
   }
 }
