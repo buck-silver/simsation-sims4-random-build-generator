de8509050b7aa3f0787658731a25c355b290d511
kevdevbot <kevdevbot@gmail.com>
2021-01-17 16:02:29 -0700

    Updated specials data and conditions

--- src/app/services/sims4-build-specials.service.ts
+++ src/app/services/sims4-build-specials.service.ts
@@ -3,7 +3,7 @@ import { Sims4BuildPackService } from './sims4-build-pack.service';
 import { getRandomInt } from '../util/get-random-int';
 
 class SpecialsConditions {
-  constructor(public pack: string = '', public room: string = '') {}
+  constructor(public pack: string = '', public specials: string[] = []) {}
 }
 
 @Injectable({
@@ -12,55 +12,138 @@ class SpecialsConditions {
 export class Sims4BuildSpecialsService {
   constructor(sims4packs: Sims4BuildPackService) {
     sims4packs.packs.subscribe((val) => {
-      const updatedRooms = [];
+      const updatedSpecials = [];
+      const duplicates = new Set<string>();
 
-      for (const condition of this.roomsWithConditions) {
-        if (val.get(condition.pack)?.enabled) {
-          updatedRooms.push(condition.room);
+      for (const condition of this.specialsWithConditions) {
+        // Check if pack is enabled
+        if (val.get(condition.pack)?.enabled == true) {
+          for (const special of condition.specials) {
+            // If no duplicate specials have already been added, add to special list
+            if (!duplicates.has(special)) {
+              updatedSpecials.push(special);
+              duplicates.add(special);
+            }
+          }
         }
       }
-
-      this.rooms = [...updatedRooms, ...this.baseRooms];
+      this.specials = [...updatedSpecials, ...this.baseSpecials];
     });
   }
 
-  rooms: string[] = [];
+  specials: string[] = [];
 
-  private baseRooms: string[] = [
-    'an Art Studio or Gallery',
-    'an Attic',
-    'a Bar',
-    'a Basement',
-    'a Craft Room',
-    'a DJ or Music Room',
-    'a Food & Flower Garden',
+  private baseSpecials: string[] = [
+    'Art Studio or Gallery',
+    'Attic',
+    'Bar',
+    'Basement',
+    'Craft Room',
+    'DJ or Music Room',
+    'Food & Flower Garden',
     'Fruit Trees',
-    'a Game Room',
-    'a Garage or Workshop',
-    'a Garden Shed',
-    'a Greenhouse',
-    'a Guest Bedroom',
-    'a Home Gym',
-    'a Home Theatre',
-    'a Hot Tub',
-    'a Huge Food Farm',
-    'a Kids Play Room',
-    'an Office',
-    'a Patio with BBQ',
-    'a Pool',
-    'a Small Flower Garden',
-    'a Small Food Garden',
-    'a Sun or Plant Room',
+    'Game Room',
+    'Garage or Workshop',
+    'Garden Shed',
+    'Greenhouse',
+    'Guest Bedroom',
+    'Home Gym',
+    'Home Theatre',
+    'Hot Tub',
+    'Huge Food Farm',
+    'Kids Play Room',
+    'Office',
+    'Patio with BBQ',
+    'Pool',
+    'Small Flower Garden',
+    'Small Food Garden',
+    'Sun or Plant Room',
+    'Cemetary or Mausoleum',
+    'Shrine to the Tragic Clown',
+    'Playground Equipment',
+    'Fountain',
+    'Outhouse',
+    'Skylight or Glass Roof',
+    'Rocketship',
+    'Decorate for Christmas',
+    'Nursery',
+    'Motion Gaming Rig',
+    'Cupcake Machine',
+    'Coffee or Tea Machine',
   ];
 
-  private roomsWithConditions: SpecialsConditions[] = [
-    new SpecialsConditions('City Living', 'a Basketball Court'),
-    new SpecialsConditions('Bowling Night', 'a Bowling Alley'),
-    new SpecialsConditions('Spa Day', 'a Massage Room'),
-    new SpecialsConditions('Spa Day', 'a Sauna'),
+  private specialsWithConditions: SpecialsConditions[] = [
+    new SpecialsConditions('City Living', [
+      'Basketball Court',
+      'Karaoke Machine',
+      'Bubble Blower Machine',
+    ]),
+    new SpecialsConditions('Bowling Night', ['Bowling Alley']),
+    new SpecialsConditions('Spa Day', ['Massage Room', 'Sauna', 'Yoga Room']),
+    new SpecialsConditions('Get Famous', [
+      'Money Vault',
+      'Video Station',
+      'Streaming Drone',
+    ]),
+    new SpecialsConditions('Get Together', ['Arcade Machine', 'Woohoo Bush']),
+    new SpecialsConditions('Jungle Adventure', ['Woohoo Bush']),
+    new SpecialsConditions('Seasons', [
+      'Kiddie Pool',
+      'Patchy',
+      'Bee Farm',
+      'Weather Control Device',
+    ]),
+    new SpecialsConditions('City Living', [
+      'Karaoke Machine',
+      'Bubble Blower Machine',
+    ]),
+    new SpecialsConditions('Discover University', ['Robotics Workstation']),
+    new SpecialsConditions('Eco Lifestyle', ['Dumpster', 'Insect Farm']),
+    new SpecialsConditions('Snowy Escape', ['Kotatsu Table', 'Zen Garden']),
+    new SpecialsConditions('Cats & Dogs', ['Pet Stuff']),
+    new SpecialsConditions('My First Pet', ['Pet Stuff']),
+    new SpecialsConditions('Get to Work', ['Photography Studio']),
+    new SpecialsConditions('Moschino', ['Photography Studio']),
+    new SpecialsConditions('Get Together', ['Fire Pit']),
+    new SpecialsConditions('Island Living', ['Fire Pit']),
+    new SpecialsConditions('Outdoor Retreat', ['Fire Pit']),
+    new SpecialsConditions('Realm of Magic', ['Cauldron Room']),
+    new SpecialsConditions('Vampires', ['Vampire Coffin Room']),
+    new SpecialsConditions('Journey to Batuu', ['5 Star Wars Objects']),
+    new SpecialsConditions('Parenthood', ['Family Bulletin Board']),
+    new SpecialsConditions('Dine Out', ['Chef Station']),
+    new SpecialsConditions('Cool Kitchen', ['Ice Cream Machine']),
+    new SpecialsConditions('Fitness', ['Rock Climbing Wall']),
+    new SpecialsConditions('Backyard', ["Slip n' Slide"]),
+    new SpecialsConditions('Toddler', ['Ball Pit']),
+    new SpecialsConditions('Laundry Day', ['Laundry Room']),
+    new SpecialsConditions('Romantic Garden', ['Wishing Well']),
+    new SpecialsConditions('Tiny Living', ['Murphy Bed']),
+    new SpecialsConditions('Vintage Glamour', ["Butler's Bedroom"]),
+    new SpecialsConditions('Perfect Patio', [
+      'Perfect Patio Outdoor Living Room',
+    ]),
+    new SpecialsConditions('Nifty Knitting', ['Knitting Room']),
+    new SpecialsConditions('Luxury Party', [
+      'Banquet Table',
+      'Fountain of Mirth',
+    ]),
+    new SpecialsConditions('Spooky', [
+      'Pumpkin Carving Station',
+      'Decorate for Halloween',
+    ]),
+    new SpecialsConditions('Movie Hangout', [
+      'Lilacs & Lanterns Tree',
+      'Popcorn Machine',
+    ]),
+    new SpecialsConditions('Kids Room', [
+      'Puppet Theater',
+      'Voidcritter Battle Station',
+    ]),
+    new SpecialsConditions('Paranormal', ['Séance Room']),
   ];
 
   getRandom(): string {
-    return this.rooms[getRandomInt(0, this.rooms.length - 1)];
+    return this.specials[getRandomInt(0, this.specials.length - 1)];
   }
 }
