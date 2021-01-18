cdf21746c300c177702c2855e2d266ccdfd236a4
kevdevbot <kevdevbot@gmail.com>
2021-01-18 12:24:09 -0700

    Updated pack settings  selector

--- src/app/components/sims4-pack-settings/sims4-pack-settings.component.ts
+++ src/app/components/sims4-pack-settings/sims4-pack-settings.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Sims4BuildPackService } from '../../services/sims4-build-pack.service';
 
 @Component({
-  selector: 'app-sims4-pack-settings',
+  selector: 'sims4-pack-settings',
   templateUrl: './sims4-pack-settings.component.html',
   styleUrls: ['./sims4-pack-settings.component.scss'],
 })
